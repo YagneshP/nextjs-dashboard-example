c9d732972d13dfe63df1b5387b3e60afe7d4e358
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-12 15:54:57 -0500

    Add Pagination logic

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,6 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParam); // params become iterable object like map
+    params.set("page", "1");
     if (term) {
       params.set("query", term); // you can set the query prop
     } else {
