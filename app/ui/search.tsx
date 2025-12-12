42058f46f9d0cad158b0a526519741c0519f78e1
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-12 12:42:06 -0500

    Update query for url using replace useRoute usePathName

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,32 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParam = useSearchParams();
+  const pathname = usePathname(); //
+  const { replace } = useRouter();
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParam); // params become iterable object like map
+    if (term) {
+      params.set("query", term); // you can set the query prop
+    } else {
+      params.delete("query"); // if search term is empty delete the prop
+    }
+    replace(`${pathname}?${params.toString()}`); //replace update the url with current pathname and param stringify
+  }
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
+    <div className='relative flex flex-1 flex-shrink-0'>
+      <label htmlFor='search' className='sr-only'>
         Search
       </label>
       <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <MagnifyingGlassIcon className='absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
     </div>
   );
 }
